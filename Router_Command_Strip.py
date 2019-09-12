1fe470320f4205a994ca7af88335a25d3453952e
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2019-09-12 22:53:16 +0930

    Update Router_Command_Strip.py

--- Router_Command_Strip.py
+++ Router_Command_Strip.py
@@ -8,7 +8,7 @@ __date__ = '06.09.2019'
 
 import os
 
-def line_strip():
+def line_strip(input_file, output_file):
 	"""
 
 	"""
@@ -17,6 +17,15 @@ def main():
 	"""
 	Begin
 	"""
+	if os.path.exists("router.txt"):
+		input_file = open("router.txt", "r")
+		output_file = open("new_router.txt", 'w')
+		line_strip(input_file, output_file)
+	else:
+		print("File not there")
+		input_file = open("router.txt", "w")
+	input_file.close()
+	
   
 if __name__ == '__main__':
     main()
